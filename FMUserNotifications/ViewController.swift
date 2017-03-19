abe516fec81d827352cd269df82a6f62f618077c
xiaoqianghou <2662651103@qq.com>
2017-03-19 11:25:51 +0800

    Listing 19-11: Scheduling a notification

--- FMUserNotifications/ViewController.swift
+++ FMUserNotifications/ViewController.swift
@@ -6,20 +6,30 @@
 //  Copyright © 2017年 WuQiang. All rights reserved.
 //
 
+
+
+
+// Listing 19-11: Scheduling a notification
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var messageField: UITextField!
+    
+    @IBAction func saveNotification(_ sender: UIButton) {
+        let notificationcenter = UNUserNotificationCenter.current()
+        let content = UNMutableNotificationContent()
+        content.title = "Reminder"
+        content.body = messageField.text!
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 8, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "reminder", content: content, trigger: trigger)
+        notificationcenter.add(request, withCompletionHandler: { (error) in
+            let main = OperationQueue.main
+            main.addOperation({
+                self.messageField.text = "试试"
+            })
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
